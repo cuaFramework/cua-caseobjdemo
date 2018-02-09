72b676964eba5918bdd79a0912b1ecba9a870c53
shimine <shimine@test.com>
2018-02-09 18:04:04 +0800

    整理格式

--- module/v2exapi/login_page.py
+++ module/v2exapi/login_page.py
@@ -7,7 +7,7 @@ from config.config import ENV
 
 
 class LoginPage(object):
-    def __init__(self,env='prod'):
+    def __init__(self, env='prod'):
         self.v2exapi = V2exapi(env)
 
     def get_login_params(self, env=ENV):
@@ -17,7 +17,7 @@ class LoginPage(object):
         soup = BeautifulSoup(login_page, "lxml")
         login_form = soup.find(action="/signin")
         login_param_lists = login_form.table
-        username=''
+        username = ''
         pwd = ''
         once = ''
         for param in login_param_lists:
@@ -28,14 +28,15 @@ class LoginPage(object):
                 else:
                     key = ''
 
-                if key =='用户名':
+                if key == '用户名':
                     username = param.contents[3].contents[0]['name']
-                elif key =='密码':
+                elif key == '密码':
                     pwd = param.contents[3].contents[0]['name']
                 elif key == '':
                     once = param.contents[3].contents[0]['value']
                     break
-        return dict(username=username,pwd=pwd,once=once)
+        return dict(username=username, pwd=pwd, once=once)
+
 
 if __name__ == "__main__":
     x = LoginPage()
